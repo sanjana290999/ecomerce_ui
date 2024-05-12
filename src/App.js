e5f9b1f95cca1efda25c518e5475b61e558ec49a
Your Name <you@example.com>
2024-05-12 21:46:35 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+// import Todo from "./Components/Todo";
+import HomePage from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import Login from "./Components/Login";
+import SignIn from "./Components/SignIn";
+import OtpVerify from "./Components/Otp";
+import ConfirmPassword from "./Components/ConfirmPassword";
+import ProductDetails from "./Components/ProductDetails";
+import Products from "./Components/Products";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        {/* <Route path="/" element={<Todo />} /> */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="login" element={<Login />} />
+        <Route path="sign_in" element={<SignIn />} />
+        <Route path="otp_verify" element={<OtpVerify />} />
+        <Route path="confirm_password" element={<ConfirmPassword />} />
+        <Route path="product_details/:productId" element={<ProductDetails />} />
+        <Route path="product_details/:productId" element={<ProductDetails />} />
+        <Route path="products" element={<Products />} />
+      </Routes>
     </div>
   );
 }
